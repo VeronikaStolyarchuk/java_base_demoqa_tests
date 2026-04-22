1479a326a78feab689ad032c5a268342f7452061
VeronikaStolyarchuk <veronika.stolyarchuk@yandex.ru>
2026-04-22 20:59:58 +0300

    Added BaseTest

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,24 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class BaseTest {
+    @BeforeAll
+    static void BeforeAll() {
+        Configuration.browser = "chrome";
+        Configuration.browserVersion = "144.0";
+        Configuration.browserSize = "1920*1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.timeout = 10000; //default=4000
+    }
+
+    @AfterEach
+    void afterEach() {
+        closeWebDriver();
+    }
+}
